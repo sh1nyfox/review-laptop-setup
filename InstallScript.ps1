5a5731994da237c15a8a21f3015b6c0907996fed
sh1nyfox <sh1nyfox@protonmail.com>
2020-10-10 19:06:04 +0100

    Added Games

--- InstallScript.ps1
+++ InstallScript.ps1
@@ -2,5 +2,15 @@
 
 # Install Chocolatey 
 
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Write-Output "Installing Chocolatey"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    choco install chocolatey-core.extension -y
 
+##################
+#Install programs#
+##################
+
+##Gaming
+    choco install steam #Steam
+    choco install uplay #UPlay
+    choco install epicgameslauncher #Epic        
