d7ed304aba09728f1e606fe8c92787b4cb760b9e
sh1nyfox <sh1nyfox@protonmail.com>
2020-10-10 18:16:08 +0100

    First script text

---
+++ InstallScript.ps1
@@ -0,0 +1,5 @@
+# Set execution policy, install Chocolatey, then all required packages 
+
+# Set execution policy as administrator
+runas /user:Administrator Set-ExecutionPolicy Unrestricted -Force
+
