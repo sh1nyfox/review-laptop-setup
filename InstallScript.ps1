2d22bd324114f6cd18b071645d827783e9347b13
sh1nyfox <sh1nyfox@protonmail.com>
2020-10-15 14:46:08 +0100

    Added Google Chrome and text messages

--- InstallScript.ps1
+++ InstallScript.ps1
@@ -2,23 +2,29 @@
 
 # Install Chocolatey
 
-    Write-Output "Installing Chocolatey"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    choco install chocolatey-core.extension -y
+  Write-Output "Installing Chocolatey"
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  choco install chocolatey-core.extension -y
 
 ##################
 #Install programs#
 ##################
 
 ##Gaming
-    choco install steam -y #Steam
-    choco install uplay -y #UPlay
-    choco install epicgameslauncher -y #Epic
-    choco install origin -y #Origin
+  Write-Output "Installing Game Launchers"
+  choco install steam -y #Steam
+  choco install uplay -y #UPlay
+  choco install epicgameslauncher -y #Epic
+  choco install origin -y #Origin
 
 ##Benchmarks
-    choco install crystaldiskmark -y #CrystalDiskMark
-    choco install cpu-z -y #CPU-Z
-    choco install gpu-z -y #GPU-Z
-    choco install pci-z -y #PCI-Z
-    choco install cinebench #Cinebench R20
+  Write-Output "Installing Benchmark Apps"
+  choco install crystaldiskmark -y #CrystalDiskMark
+  choco install cpu-z -y #CPU-Z
+  choco install gpu-z -y #GPU-Z
+  choco install pci-z -y #PCI-Z
+  choco install cinebench #Cinebench R20
+
+## Direct install of Google Chrome
+  Write-Output "Installing Google Chrome"
+  $Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-A WIP script to help me setup review laptops for my day job a lot quicker. 
+A WIP script to help me setup review laptops for my day job a lot quicker.
 
 Any advice on how to streamline the process will be greatfully received. I'm a complete noob but I'm starting to learn!
 
-Current plan: 
+Current plan:
 
 - Install Chocolatey
 - Pull various packages for gaming, benchmarking, everyday use
 - Split off forks for different types of laptop (gaming, Ultrabooks etc)
-<!--stackedit_data:
-eyJoaXN0b3J5IjpbMTA3Mjk3Mjk5NV19
--->
+
+Future plan:
+
+- Run Windows Update from the same install script *after* app installs
