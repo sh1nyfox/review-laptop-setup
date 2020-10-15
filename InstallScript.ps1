bdf0a98dbca856fecbcf857d24d59c262004de35
sh1nyfox <sh1nyfox@protonmail.com>
2020-10-15 14:30:02 +0100

    Added cinebench r20

--- InstallScript.ps1
+++ InstallScript.ps1
@@ -1,6 +1,6 @@
-# Install Chocolatey, then all required packages 
+# Install Chocolatey, then all required packages
 
-# Install Chocolatey 
+# Install Chocolatey
 
     Write-Output "Installing Chocolatey"
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -13,11 +13,12 @@
 ##Gaming
     choco install steam -y #Steam
     choco install uplay -y #UPlay
-    choco install epicgameslauncher -y #Epic       
+    choco install epicgameslauncher -y #Epic
     choco install origin -y #Origin
-    
+
 ##Benchmarks
     choco install crystaldiskmark -y #CrystalDiskMark
     choco install cpu-z -y #CPU-Z
     choco install gpu-z -y #GPU-Z
     choco install pci-z -y #PCI-Z
+    choco install cinebench #Cinebench R20
